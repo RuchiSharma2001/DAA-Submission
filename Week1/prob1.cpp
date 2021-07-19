3e14262058e2dddc577f0a9516426281660b4aff
Ruchi Sharma <ruchisharma2460@gmail.com>
2021-07-19 11:27:30 +0530

    Linear Search

---
+++ Week1/prob1.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int t,n,k,i;
+    cin>>t;
+    while(t--){
+        cin>>n;
+        int a[n];
+        for(i=0;i<n;i++){
+            cin>>a[i];
+        }
+        cin>>k;
+        for(i=0;i<n;i++){
+            if(a[i]==k){
+                break;
+            }
+        }
+        if(i<n)
+            cout<<"Present "<<i+1;
+        else
+            cout<<"Not Present"<<i+1;
+    }
+    return 0;
+}
